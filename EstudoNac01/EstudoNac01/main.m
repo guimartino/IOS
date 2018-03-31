8da984b74c257a27caa2aaf0fc7ff3513f8dd29b
Guilherme Martino <gui.martino@hotmail.com>
2018-03-31 14:07:26 -0300

    First commit

---
+++ EstudoNac01/EstudoNac01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EstudoNac01
+//
+//  Created by Guilherme Martino on 31/03/2018.
+//  Copyright © 2018 Guilherme Martino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
